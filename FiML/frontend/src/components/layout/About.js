66b54428e2f0e55e73a26f7f8cac9519040ed790
ongwaihong <samuelong168@gmail.com>
2021-01-02 20:22:21 +0000

    add demo vid on about page

--- FiML/frontend/src/components/layout/About.js
+++ FiML/frontend/src/components/layout/About.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Container from '@material-ui/core/Container';
 import {Link} from "react-router-dom"
 import { connect } from 'react-redux'
+import ReactPlayer from "react-player"
 
 function About({user}) {
     var loginLine = null
@@ -18,6 +19,13 @@ function About({user}) {
             <h5>FiML uses a combination of implicit and explicit collaborative filtering based on state-of-the-art matrix factorization techniques, to bring you the very best film recommendations!
             </h5>
             {loginLine}
+            <h5>Check out this demo if you're unconvinced!</h5>
+            <div style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+            <ReactPlayer
+                url="https://vimeo.com/496438347"
+            />
+            </div>
+
         </Container>
     )
 }
